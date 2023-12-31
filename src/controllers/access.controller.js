28c5fe4073266dc0454a037727b724c3eddae55c
AndyTang <tangthienan9@gmail.com>
2023-12-31 12:00:43 +0700

    error + success handler

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,19 +1,18 @@
 "use strict";
 
+const { CREATED } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
     signUp = async (req, res, next) => {
-        try {
-            console.log(`[P]::signUp::`, req.body);
-            /**
-             * 200 OK
-             * 201 CREATED
-             */
-            return res.status(201).json(await AccessService.signUp(req.body));
-        } catch (error) {
-            next(error);
-        }
+        new CREATED({
+            message: "Registered OK!",
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 10,
+            },
+        }).send(res);
+        // return res.status(201).json(await AccessService.signUp(req.body));
     };
 }
 
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -6,6 +6,7 @@ const crypto = require("crypto");
 const KeyTokenService = require("./keyToken.service");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const { BadRequestError } = require("../core/error.response");
 
 const RoleShop = {
     SHOP: "SHOP",
@@ -16,119 +17,108 @@ const RoleShop = {
 
 class AccessService {
     static signUp = async ({ name, email, password }) => {
-        console.log({ name, email, password });
-        try {
-            //.lean() return a pure js object which lighter than without lean() ~30 times
-            const holderShop = await shopModel.findOne({ email }).lean();
+        //.lean() return a pure js object which lighter than without lean() ~30 times
+        const holderShop = await shopModel.findOne({ email }).lean();
 
-            if (holderShop) {
-                return {
-                    code: "XXXX",
-                    message: "shop already registered",
-                };
-            }
+        if (holderShop) {
+            throw new BadRequestError("Error: Shop already registered!");
+        }
 
-            const passwordHash = await bcrypt.hash(password, 10);
-            const newShop = await shopModel.create({
-                name,
-                email,
-                password: passwordHash,
-                roles: [RoleShop.SHOP],
+        const passwordHash = await bcrypt.hash(password, 10);
+        const newShop = await shopModel.create({
+            name,
+            email,
+            password: passwordHash,
+            roles: [RoleShop.SHOP],
+        });
+
+        if (newShop) {
+            // created privateKey, publicKey base on asymmetric cryptography
+
+            /**
+             * privateKey: stored on user side (not on server side), this used for sign token
+             * publicKey: stored on server side, this used for verify token
+             * Ex: incase Hacker connect to our db, they just have publicKey which use for verification only.
+             */
+
+            /**
+             * This is the hardcore privateKey, publicKey version
+             * -->
+             * const { privateKey, publicKey } = crypto.generateKeyPairSync(
+             *     "rsa",
+             *     {
+             *         modulusLength: 4096,
+             *         publicKeyEncoding: {
+             *             type: "pkcs1",
+             *             format: "pem",
+             *         },
+             *         privateKeyEncoding: {
+             *             type: "pkcs1",
+             *             format: "pem",
+             *         },
+             *     }
+             * );
+             */
+
+            const privateKey = crypto.randomBytes(64).toString("hex");
+            const publicKey = crypto.randomBytes(64).toString("hex");
+
+            console.log({ privateKey, publicKey }); // save to collection KeyStore
+
+            /**
+             * privateKey, publicKey format: {
+             *   privateKey: '17054c0ad1b5ac3ef4ac0d787abacf90aab8f117079aacc12f5a3f5e26f038aa2fbc48d5c9b0fd178e0f899620a4a4f08bd0dd61c4cb5556b4b5321040199171',
+             *   publicKey: 'a596ef6047a921b9f2159c15bf01e4e22f995f36ed18f40bfd67dd38e6752f2a85b5a3e0acbf51cf53707a7743bc337e4178bd43072cc46a64f76a723413a8f5'
+             * }
+             */
+
+            /**
+             * publicKey: rsa format - cannot store to mongodb
+             * publicKeyString: publicKey.string()
+             * publicKeyObject: encode publicKeyString to publicKey
+             */
+
+            // Store key to mongoDb
+            const keyStore = await KeyTokenService.createKeyToken({
+                userId: newShop._id,
+                publicKey,
+                privateKey,
             });
 
-            if (newShop) {
-                // created privateKey, publicKey base on asymmetric cryptography
-
-                /**
-                 * privateKey: stored on user side (not on server side), this used for sign token
-                 * publicKey: stored on server side, this used for verify token
-                 * Ex: incase Hacker connect to our db, they just have publicKey which use for verification only.
-                 */
-
-                /**
-                 * This is the hardcore privateKey, publicKey version
-                 * -->
-                 * const { privateKey, publicKey } = crypto.generateKeyPairSync(
-                 *     "rsa",
-                 *     {
-                 *         modulusLength: 4096,
-                 *         publicKeyEncoding: {
-                 *             type: "pkcs1",
-                 *             format: "pem",
-                 *         },
-                 *         privateKeyEncoding: {
-                 *             type: "pkcs1",
-                 *             format: "pem",
-                 *         },
-                 *     }
-                 * );
-                 */
-
-                const privateKey = crypto.randomBytes(64).toString("hex");
-                const publicKey = crypto.randomBytes(64).toString("hex");
-
-                console.log({ privateKey, publicKey }); // save to collection KeyStore
-
-                /**
-                 * privateKey, publicKey format: {
-                 *   privateKey: '17054c0ad1b5ac3ef4ac0d787abacf90aab8f117079aacc12f5a3f5e26f038aa2fbc48d5c9b0fd178e0f899620a4a4f08bd0dd61c4cb5556b4b5321040199171',
-                 *   publicKey: 'a596ef6047a921b9f2159c15bf01e4e22f995f36ed18f40bfd67dd38e6752f2a85b5a3e0acbf51cf53707a7743bc337e4178bd43072cc46a64f76a723413a8f5'
-                 * }
-                 */
-
-                /**
-                 * publicKey: rsa format - cannot store to mongodb
-                 * publicKeyString: publicKey.string()
-                 * publicKeyObject: encode publicKeyString to publicKey
-                 */
-
-                // Store key to mongoDb
-                const keyStore = await KeyTokenService.createKeyToken({
-                    userId: newShop._id,
-                    publicKey,
-                    privateKey,
-                });
-
-                if (!keyStore) {
-                    return {
-                        code: "xxxx",
-                        message: "keyStore error",
-                    };
-                }
-
-                // created token pair
-                const tokens = await createTokenPair(
-                    { userId: newShop._id, email },
-                    publicKey,
-                    privateKey
-                );
-
-                console.log(`Token created Success::`, tokens);
-
+            if (!keyStore) {
+                // throw new BadRequestError('Error: ')
                 return {
-                    code: 201,
-                    metadata: {
-                        shop: getInfoData({
-                            fields: ["_id", "name", "email"],
-                            object: newShop,
-                        }),
-                        tokens,
-                    },
+                    code: "xxxx",
+                    message: "keyStore error",
                 };
-                // const tokens = await
             }
 
+            // created token pair
+            const tokens = await createTokenPair(
+                { userId: newShop._id, email },
+                publicKey,
+                privateKey
+            );
+
+            console.log(`Token created Success::`, tokens);
+
             return {
-                code: 200,
-                metadata: null,
+                code: 201,
+                metadata: {
+                    shop: getInfoData({
+                        fields: ["_id", "name", "email"],
+                        object: newShop,
+                    }),
+                    tokens,
+                },
             };
-        } catch (error) {
-            return {
-                code: "xxx",
-                message: error.message,
-                status: "error",
-            };
+            // const tokens = await
         }
+
+        return {
+            code: 200,
+            metadata: null,
+        };
     };
 }
 
